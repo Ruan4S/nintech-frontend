f174ed2c33b58702f7ead3c46a5b4d8839bd1e92
Ruan Araújo <ruands2002@gmail.com>
2022-04-07 01:04:25 -0300

    fix: correções scroll anchor na mudança de rotas e refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
@@ -8,6 +8,7 @@ import { filter, map, switchMap } from 'rxjs/operators';
 import { environment } from '@env/environment';
 import { Logger, UntilDestroy, untilDestroyed } from '@shared';
 import { I18nService } from '@app/i18n';
+import { ViewportScroller } from '@angular/common';
 
 const log = new Logger('App');
 
@@ -17,14 +18,15 @@ const log = new Logger('App');
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
   isLoading: boolean = true;
   constructor(
     private router: Router,
-    private activatedRoute: ActivatedRoute,
+    private route: ActivatedRoute,
     private titleService: Title,
     private translateService: TranslateService,
-    private i18nService: I18nService
+    private i18nService: I18nService,
+    private readonly viewPortScroller: ViewportScroller
   ) {}
 
   ngOnInit() {
@@ -44,7 +46,7 @@ export class AppComponent implements OnInit, OnDestroy {
     merge(this.translateService.onLangChange, onNavigationEnd)
       .pipe(
         map(() => {
-          let route = this.activatedRoute;
+          let route = this.route;
           while (route.firstChild) {
             route = route.firstChild;
           }
@@ -64,6 +66,30 @@ export class AppComponent implements OnInit, OnDestroy {
     setTimeout(() => {
       this.isLoading = false;
     }, 1000);
+
+    // XXX: Corrige scroll no refresh da pagina com fragment
+    setTimeout(() => {
+      this.route.fragment.subscribe({
+        next: (fragment) => {
+          if (fragment) {
+            this.viewPortScroller.scrollToAnchor(fragment);
+          }
+        },
+      });
+    }, 1500);
+  }
+
+  ngAfterViewInit(): void {
+    // XXX: Corrige scroll na troca de uma rota sem fragment para outra com
+    this.route.fragment.subscribe({
+      next: (fragment) => {
+        if (fragment) {
+          setTimeout(() => {
+            this.viewPortScroller.scrollToAnchor(fragment);
+          }, 0);
+        }
+      },
+    });
   }
 
   ngOnDestroy() {
