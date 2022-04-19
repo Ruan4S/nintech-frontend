f8da5f772d2383884f0a915fec1b9aaf1a05fd2e
Ruan Araújo <ruands2002@gmail.com>
2022-04-18 23:25:04 -0300

    feat(Projetos): estrutura + estilização ok

---
+++ src/app/projetos/projeto/projeto.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projeto',
+  templateUrl: './projeto.component.html',
+  styleUrls: ['./projeto.component.scss'],
+})
+export class ProjetoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/projetos/projetos.module.ts
+++ src/app/projetos/projetos.module.ts
@@ -2,9 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ProjetosRoutingModule } from './projetos-routing.module';
 import { ProjetosComponent } from './projetos/projetos.component';
+import { FiltroComponent } from './filtro/filtro.component';
+import { ProjetoComponent } from './projeto/projeto.component';
+import { MaterialModule } from '@app/material.module';
 
 @NgModule({
-  declarations: [ProjetosComponent],
-  imports: [CommonModule, ProjetosRoutingModule],
+  imports: [CommonModule, ProjetosRoutingModule, MaterialModule],
+  declarations: [ProjetosComponent, FiltroComponent, ProjetoComponent],
+  exports: [ProjetosComponent, FiltroComponent, ProjetoComponent],
 })
 export class ProjetosModule {}
