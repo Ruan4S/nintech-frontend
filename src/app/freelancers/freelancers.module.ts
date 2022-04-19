7f504786cf31e541650966940f1b9c92215b89d0
Ruan Araújo <ruands2002@gmail.com>
2022-04-18 22:54:09 -0300

    feat(Freelancers): estrutura + estilo ok

--- src/app/freelancers/freelancers.module.ts
+++ src/app/freelancers/freelancers.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { MaterialModule } from '@app/material.module';
+import { TranslateModule } from '@ngx-translate/core';
 import { FreelancersRoutingModule } from './freelancers-routing.module';
 import { FreelancersComponent } from './freelancers/freelancers.component';
+import { FiltroComponent } from './filtro/filtro.component';
+import { ProfissionalComponent } from './profissional/profissional.component';
 
 @NgModule({
   imports: [CommonModule, TranslateModule, FlexLayoutModule, MaterialModule, FreelancersRoutingModule],
-  declarations: [FreelancersComponent],
+  declarations: [FreelancersComponent, FiltroComponent, ProfissionalComponent],
 })
 export class FreelancersModule {}
